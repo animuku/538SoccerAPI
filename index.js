e92af35f950c46d58f964fd8da2ce0586464ec6f
Anirudh <animuku123@gmail.com>
2021-01-19 21:35:26 -0500

    Added ZSET indexing and endpoints for retrieving rankings

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const express = require("express")
 const path = require("path")
+const Redis = require("ioredis");
+const redis = new Redis(6379,"127.0.0.1");
 const app = express()
 const port = 3000
+
+app.get("/html",(req,res)=>{
+    res.sendFile(path.join(__dirname)+"/public/homepage.html")
+})
 
 app.get("/",(req,res)=>{
     res.send("Hello,World")
 })
 
-app.get("/documentation",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./static/template.html"))
+app.get("/team",(req,res)=>{
+    const team = req.query.team;
+    redis.hgetall(team)
+    .then((data)=>{
+        res.json(data)
+    });
 })
 
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`)
+app.get("/globalrankings",async(req,res)=>{
+    var response = {}
+    var results = [];
+    response.status = "OK"
+    const top = req.query.top;
+    let teams = await redis.zrange("sortedBySPI",0,top-1);
+    for(entry of teams)
+    {
+        let obj = await redis.hgetall(entry)
+        results.push(obj)
+    }
+    response.status = "OK"
+    response.results = results
+    res.json(response)
 })
+
+app.post("/",(res,req)=>{
+    res.send("Got a PUT request")
+})
+
+
+app.listen(port,()=>{
+    console.log(`Example app listening at port ${port}`)
+})
+
--- populate.py
+++ populate.py
@@ -1,19 +1,17 @@
 import redis
 import pandas as pd
 r = redis.Redis(
-    host='localhost',
+    host='127.0.0.1',
     port = 6379,
 )
 data = pd.read_csv('./soccer-spi/spi_global_rankings.csv')
 data = data.drop(['prev_rank'],axis=1)
 for row in data.values:
     mapping = {}
+    mapping["id"] = row[1]
     mapping["league"] = row[2]
     mapping["off"] = row[3]
     mapping["def"] = row[4]
     mapping["spi"] = row[5]
     r.hmset(row[1],mapping)
-
-
-for row in data.values:
-    
+    r.zadd("sortedBySPI",{row[1]:-row[5]})
